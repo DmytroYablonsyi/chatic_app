6ac5bf86abf62b461aab7bf825266c93bc544eeb
DmytroYablonsyi <yablonskiyd21@gmail.com>
2025-06-18 15:26:05 +0300

    added some fix

--- backend/src/server.js
+++ backend/src/server.js
@@ -13,13 +13,12 @@ const PORT = process.env.PORT;
 const __dirname = path.resolve();
 
 app.use(cors({
-    origin: ["http://localhost:5173"],
+    origin: "http://localhost:5173",
     credentials: true
 }))
 
 app.use(express.json());
 app.use(cookieParser());
-
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
